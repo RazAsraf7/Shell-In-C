15e15a09a5847be22af59334e649d61177d2fc64
Raz Asraf <razasraf7@gmail.com>
2026-04-14 00:19:07 +0300

    Absolute path & no path works

--- shell.c
+++ shell.c
@@ -6,18 +6,12 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-//char * path = getenv();
-
 
 int main(){
 	char command[200], new_command[200], env2[200], *token, *env;
 	int pid, stat;
 	env = getenv("PATH");
-	token = strtok(env,":");
-	while(token != NULL){
-		printf("the token is: %s\n",token);
-		token = strtok(NULL,":");
-	}
+	
 	while(true){
 		printf("Please put your command: ");
 		fgets(command, 200,stdin);
@@ -26,7 +20,17 @@ int main(){
 
 		pid = fork();
 		if (pid == 0){
-			execl(command,command,NULL);
+			token = strtok(env,":");
+			while(token != NULL){
+				strcpy(new_command,token);
+				strcat(new_command,"/");
+				strcat(new_command,command);
+				token = strtok(NULL,":");
+				char * arr[] = {new_command,NULL};
+				execv(arr[0],arr);
+			}
+			char * arr[] = {command,NULL};
+			execv(arr[0],arr);
 			printf("No such command %s, please try a different command\n",command);
 			exit(1);
 		}
